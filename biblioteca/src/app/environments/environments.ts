7a355fcd45aa43e84c7c3d45fd74d98d95ea3fb7
jher10026 <1002620232@unajma.edu.pe>
2025-11-11 21:45:49 +0000

    uno

---
+++ biblioteca/src/app/environments/environments.ts
@@ -0,0 +1,12 @@
+export const environments={
+    production : false,
+    firebaseconfig: {
+  apiKey: "AIzaSyCNp_jRz6srDTh-35LuT2U5TshYs-Lw3Mc",
+  authDomain: "biblioteca-91d43.firebaseapp.com",
+  projectId: "biblioteca-91d43",
+  storageBucket: "biblioteca-91d43.firebasestorage.app",
+  messagingSenderId: "989389936247",
+  appId: "1:989389936247:web:45528b5adb8d04c62c1872",
+  measurementId: "G-MBRSH6SZWX"
+    }
+}
